b26913d8b34ac9c9e1affd5acc5d24539d042eae
jigeorge <jierge@adobe.com>
2020-04-06 23:52:25 +0530

    Flake8 and Travis CI

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,9 +85,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
+
     {
+
         'NAME':
         'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        
     },
 
     {
